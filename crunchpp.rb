fb04221c702a13799baa5e2ecc1fd7273caff6c8
Matthias Dörfelt <hi@mokafolio.de>
2016-05-23 15:59:14 -0700

    added checksum for crunch

--- crunchpp.rb
+++ crunchpp.rb
@@ -3,7 +3,7 @@ class Crunchpp < Formula
   homepage ""
   url "https://github.com/mokafolio/Crunch/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "ed6c08adfefe27ca0843c612f68a513a7fef3ddcf1559d4ba4cbf0fad4250d76"
 
   depends_on "cmake" => :build
   depends_on "stick"
