05dd61c5eb0700573c7f8825aebd9bf02701469a
Matthias Dörfelt <hi@mokafolio.de>
2016-05-23 15:18:58 -0700

    added checksum to scrubpp.rb

--- scrubpp.rb
+++ scrubpp.rb
@@ -3,7 +3,7 @@ class Scrubpp < Formula
   homepage ""
   url "https://github.com/mokafolio/Scrub/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "0688b1ca2d512b7a0d285366d9510dadcd7ddb65a65c9e6633e1a9e3296255e9"
 
   depends_on "cmake" => :build
   depends_on "stick"
