9e0d4bde0a999d15a6a80002583e5773f962e62f
Matthias Dörfelt <hi@mokafolio.de>
2016-05-23 15:29:42 -0700

    added brick

---
+++ brick.rb
@@ -0,0 +1,14 @@
+class Brick < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/mokafolio/Brick/archive/0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 ""
+
+  depends_on "cmake" => :build
+  depends_on "stick"
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
