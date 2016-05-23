cce42302c74d95c0ada0c63ee4e16e29dab56b87
Matthias Dörfelt <hi@mokafolio.de>
2016-05-23 15:39:47 -0700

    added checksum for brick

--- brick.rb
+++ brick.rb
@@ -3,7 +3,7 @@ class Brick < Formula
   homepage ""
   url "https://github.com/mokafolio/Brick/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "7afe0f2060c4fd5c920bf7dd604bc984093b2f56b6a1de787a3e296bb841084b"
 
   depends_on "cmake" => :build
   depends_on "stick"
