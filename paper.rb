ad6cd17050d50908179abb2eacfcb7bf0a496538
Matthias Dörfelt <hi@mokafolio.de>
2016-05-23 17:23:54 -0700

    added checksum to paper

--- paper.rb
+++ paper.rb
@@ -3,7 +3,7 @@ class Paper < Formula
   homepage ""
   url "https://github.com/mokafolio/Paper/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "26906fb47e636ce84399bb68d6fb2ddf169d614dcf6ca0683022c235e18e4eac"
 
   depends_on "cmake" => :build
   depends_on "brick"
