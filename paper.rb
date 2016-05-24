ff8c54dc4d990c3b2125dedf62ac7fc716196d2f
Matthias Dörfelt <hi@mokafolio.de>
2016-05-24 11:36:13 -0700

    updated paper formula

--- paper.rb
+++ paper.rb
@@ -1,9 +1,9 @@
 class Paper < Formula
   desc ""
   homepage ""
-  url "https://github.com/mokafolio/Paper/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "26906fb47e636ce84399bb68d6fb2ddf169d614dcf6ca0683022c235e18e4eac"
+  url "https://github.com/mokafolio/Paper/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 ""
 
   depends_on "cmake" => :build
   depends_on "brick"
