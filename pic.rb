51f18aa255a4c3a0e9292490641416ef55ca4586
Matthias Dörfelt <hi@mokafolio.de>
2016-05-26 15:45:47 -0700

    added checksum to pic formula

--- pic.rb
+++ pic.rb
@@ -3,7 +3,7 @@ class Pic < Formula
   homepage ""
   url "https://github.com/mokafolio/Pic/archive/0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "6e41d26db9ff51618e7b431b71fc6e460a8300eb8ddb7f9880b86619bdf60392"
 
   depends_on "cmake" => :build
   depends_on "stick"
