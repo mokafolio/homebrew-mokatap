91546c5b9316c62ff6f6c9e65e59e2b2273ebf33
Matthias Dörfelt <hi@mokafolio.de>
2016-05-26 15:48:16 -0700

    updated stick formula to version 0.0.2

--- stick.rb
+++ stick.rb
@@ -1,9 +1,9 @@
 class Stick < Formula
   desc ""
   homepage ""
-  url "https://github.com/mokafolio/Stick/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "dff705747ce8e9bd26f688772aa1d8d407c6173d010e8e2b6e0c56392401fb94"
+  url "https://github.com/mokafolio/Stick/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 ""
 
   depends_on "cmake" => :build
 
